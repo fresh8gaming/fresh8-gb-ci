eadd61221f425788db3ccff42d2b189421345eca
Chloe Parkes <chloe.parkes@connected-ventures.com>
2017-01-19 14:38:05 +0000

    Addition of tests for the pass, and the fail scenario

--- go_processes/tests/code_coverage_test.py
+++ go_processes/tests/code_coverage_test.py
@@ -11,18 +11,27 @@ class TestCodeCoverage(unittest.TestCase):
 
     @patch('go_processes.code_coverage.CodeCoverage._run_tests')
     @patch('go_processes.code_coverage.CodeCoverage._log_results')
-    def test_get_coverage(self, log_results_patch, run_tests_patch):
-        run_tests_patch.return_value = (self._get_test_output(), "Stuff")
+    def test_get_coverage_fail(self, log_results_patch, run_tests_patch):
+        run_tests_patch.return_value = (self._get_test_output_fail(), "Stuff")
 
         cc = CodeCoverage(self._mock_config())
         err = cc.get_coverage("./f8-jeeves", False)
 
         self.assertTrue(err)
         log_results_patch\
-            .assert_called_with(False, 25.0,
-                                "/f8-jeeves has no tests.\n"
-                                "/f8-jeeves/environment has no tests.\n"
-                                "/f8-jeeves/service under coverage threshold at 0.0%\n")  # NOQA
+            .assert_called_with(True, 25.0,
+                                '/f8-jeeves has no tests.\n/f8-jeeves/environment has no tests.\n/f8-jeeves/service under coverage threshold at 0.0%\n/f8-jeeves/mypackage FAILED')  # NOQA
+
+    @patch('go_processes.code_coverage.CodeCoverage._run_tests')
+    @patch('go_processes.code_coverage.CodeCoverage._log_results')
+    def test_get_coverage_pass(self, log_results_patch, run_tests_patch):
+        run_tests_patch.return_value = (self._get_test_output_pass(), "Stuff")
+
+        cc = CodeCoverage(self._mock_config())
+        err = cc.get_coverage("./f8-jeeves", False)
+
+        self.assertFalse(err)
+        log_results_patch.assert_called_with(False, 100.0, "")
 
     def _mock_config(self):
         mock_coverage = Mock()
@@ -37,8 +46,12 @@ class TestCodeCoverage(unittest.TestCase):
         mock_config.all = mock_project_type
         return mock_config
 
-    def _get_test_output(self):
+    def _get_test_output_fail(self):
         return "?   	fresh8.co/f8-jeeves	[no test files]\n?   	fresh8.co/f8-jeeves/environment	[no test files]\nok  	fresh8.co/f8-jeeves/service	0.019s	coverage: 0.0% of statements\nFAIL  	fresh8.co/f8-jeeves/mypackage	0.019s\nok  	fresh8.co/f8-jeeves/service/apierrors	0.022s	coverage: 100.0% of statements"  # NOQA
+
+    def _get_test_output_pass(self):
+        return "ok  	fresh8.co/f8-jeeves/service/apierrors" \
+               "	0.022s	coverage: 100.0% of statements"
 
 
 if __name__ == '__main__':
